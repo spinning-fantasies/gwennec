0947d86313e75420a23b9e27174e4d94fef08538
Mathieu Mescam <mate@e.email>
2023-09-05 19:11:34 +0200

    Add total expenses

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for 
 import sqlite3
-import pdb
+
 from datetime import datetime
 
 app = Flask(__name__)
@@ -12,10 +12,13 @@ def calendar():
 
     cursor.execute("SELECT date, payee, amount FROM expense")
     expense_data = cursor.fetchall()
-    
+
+    cursor.execute("SELECT SUM(amount) FROM expense;")
+    total_expenses = cursor.fetchall()
+
     conn.close()
 
-    return render_template('expense.html', expense_data=expense_data)
+    return render_template('expense.html', expense_data=expense_data, total_expenses=total_expenses)
 
 if __name__ == '__main__':
     app.run(debug=True)
